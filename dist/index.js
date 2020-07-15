5aa742a1873ec549a003e3c6824337d0f4ae2005
lequangphi0611 <lequangphi0611@gmail.com>
2020-07-15 20:27:37 +0700

    apply typescript and remove youtube-dl

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const rxjs_1 = require("rxjs");
+rxjs_1.from([1, 2, 3, 4]).subscribe(console.log);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+import {from} from "rxjs";
+
+from([1, 2, 3, 4]).subscribe(console.log);
