3c31673fe66f46f493c1c35f13c336c9ad8d21da
lequangphi0611 <lequangphi0611@gmail.com>
2020-07-15 20:29:00 +0700

    remove dist file and add dist file into ignore commit

--- dist/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const rxjs_1 = require("rxjs");
-rxjs_1.from([1, 2, 3, 4]).subscribe(console.log);
-//# sourceMappingURL=index.js.map
